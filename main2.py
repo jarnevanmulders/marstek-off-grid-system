759ea4ee9675122056d800c34a87868701402db5
Jarne Van Mulders <33618569+jarnevanmulders@users.noreply.github.com>
2026-06-03 17:43:54 +0200

    update

---
+++ main2.py
@@ -0,0 +1,31 @@
+import socket
+import json
+
+MARSTEK_IP = "192.168.10.141"   # pas aan naar jouw toestel
+PORT = 30000
+
+# JSON request (status batterij)
+payload = {
+    "id": 2,
+    "method": "Bat.GetStatus",
+    "params": {"id": 0}
+}
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.settimeout(2)
+
+try:
+    # stuur request
+    sock.sendto(json.dumps(payload).encode(), (MARSTEK_IP, PORT))
+
+    # ontvang response
+    data, addr = sock.recvfrom(4096)
+
+    response = json.loads(data.decode())
+    print("Response:", response)
+
+except socket.timeout:
+    print("Geen antwoord (timeout)")
+
+finally:
+    sock.close()
