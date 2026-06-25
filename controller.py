bccb24119f0880f839d50043b290277054c45b4d
Jarne Van Mulders <33618569+jarnevanmulders@users.noreply.github.com>
2026-06-25 13:54:34 +0200

    Update controller.py

--- controller.py
+++ controller.py
@@ -159,10 +159,10 @@ try:
                     combined.update(part_2["result"])
 
                     # check offgrid power
-                    off_grid = part_2.get("offgrid_power", 0)
+                    off_grid = part_2["result"].get("offgrid_power", 0)
                     if off_grid > 32767:
                         off_grid -= 65536
-                    part_1["offgrid_power"] = off_grid
+                    part_2["result"]["offgrid_power"] = off_grid
 
                     print(off_grid)
 
