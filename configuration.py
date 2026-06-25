4056633bfd0030fcdb9a110bae2272e60506529c
Jarne Van Mulders <33618569+jarnevanmulders@users.noreply.github.com>
2026-06-25 12:11:02 +0200

    Update configuration.py

--- configuration.py
+++ configuration.py
@@ -17,18 +17,18 @@ PC_DEBUG = True
 #     LOCK_PATH = CONFIG_PATH + ".lock"
 
 
-CONFIG_PATH = "/config.yaml"
-LOCK_PATH = CONFIG_PATH + ".lock"
+CONFIG_PATH = "config.yaml"
+# LOCK_PATH = CONFIG_PATH + ".lock"
 
 def retrieve_yaml_file():
     config = {}
     # print(LOCK_PATH)
     # print(CONFIG_PATH)
     try:
-        with FileLock(LOCK_PATH):
-            if os.path.exists(CONFIG_PATH):
-                with open(CONFIG_PATH, 'r') as file:
-                    config = yaml.safe_load(file) or {}
+        # with FileLock(LOCK_PATH):
+        if os.path.exists(CONFIG_PATH):
+            with open(CONFIG_PATH, 'r') as file:
+                config = yaml.safe_load(file) or {}
     except Exception as e:
         print(f"⚠️ Error reading YAML file: {e}")
 
